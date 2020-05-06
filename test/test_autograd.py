1591a01f4079aa48b9b6339bb407cfff39d48374
anjali411 <chourdiaanjali123@gmail.com>
2020-05-06 13:38:27 -0700

    Added more autograd tests for C->C complex functions
    ghstack-source-id: e5f2618a9ce3a85a57b76f8da4c8500511b5c18e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/37856

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -4137,10 +4137,10 @@ def run_functional_checks(test_case, test_name, name, apply_fn, run_grad_checks,
         test_case.assertEqual(self_variable.size(), self_variable.grad.size())
 
 # white list for complex
-complex_list = ['t', 'view', 'reshape', 'reshape_as', 'view_as',
-                'zero_', 'clone', 'tril', 'triu', 'fill_', 'eq_', 'ne_',
-                'permute', 'squeeze', 'unsqueeze', 'chunk', 'split',
-                'split_with_sizes', 'resize', 'resize_as', 'sin', 'cos']
+complex_list = ['t', 'view', 'reshape', 'reshape_as', 'view_as', 'zero_', 'clone',
+                'tril', 'triu', 'fill_', 'eq_', 'ne_', 'permute', 'squeeze', 'unsqueeze',
+                'chunk', 'split', 'split_with_sizes', 'resize', 'resize_as', 'sin', 'cos',
+                'exp', 'mul', '__rmul__', 'div', '__rdiv__']
 
 def add_test(
         name,
