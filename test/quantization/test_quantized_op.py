18e06bf82d531b45c02d4ae83c6eede84be65592
Kimish Patel <kimishpatel@fb.com>
2020-05-06 11:50:09 -0700

    Enabled per channel quantized static linear/conv
    Summary:
    Enable channelwise quantized test on qlinear and qconv.
    Dynmaic linear to follow.
    
    Test Plan:
    pytest test/quantization/test_quantized.py
    pytest test/quantization/test_quantized_module.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 52264fc3c65cb2d63ce788ed36d18cfdda5832d2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/37622

--- test/quantization/test_quantized_op.py
+++ test/quantization/test_quantized_op.py
@@ -1932,7 +1932,6 @@ class TestQuantizedLinear(unittest.TestCase):
             # QNNPACK qlinear is flaky on MACOS. Issue #27326
             if IS_PPC or TEST_WITH_UBSAN or IS_MACOS:
                 return
-            use_channelwise = False
             use_multi_dim_input = False
             # QNNPACK supports uint8 in the kernels. In the op we shift the int8
             # weight values to uint8 to be on par with fbgemm. However, this causes
@@ -2054,7 +2053,6 @@ class TestQuantizedLinear(unittest.TestCase):
         if qengine == 'qnnpack':
             if IS_PPC or TEST_WITH_UBSAN:
                 return
-            use_channelwise = False
 
         with override_quantized_engine(qengine):
             W, (W_scale, W_zp, torch_type) = W
@@ -2320,7 +2318,6 @@ class TestQuantizedConv(unittest.TestCase):
             # QNNPACK qconv is flaky on MACOS. Issue #27326
             if IS_PPC or TEST_WITH_UBSAN or IS_MACOS:
                 return
-            use_channelwise = False
 
         input_channels = input_channels_per_group * groups
         output_channels = output_channels_per_group * groups
@@ -2378,7 +2375,6 @@ class TestQuantizedConv(unittest.TestCase):
         if qengine == 'qnnpack':
             if IS_PPC or TEST_WITH_UBSAN:
                 return
-            channelwise = False
 
         with override_quantized_engine(qengine):
             qconv_prepack = torch.ops.quantized.conv2d_prepack
